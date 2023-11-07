71456400d2cbf24b2266ec8c557f6ae737a4053e
Shruti Deshmukh <147811880+shruti15deshmukh@users.noreply.github.com>
2023-11-07 22:00:12 +0530

    committed by shruti on 7 nov 22:00

---
+++ pack_shruti_seasons/Season.java
@@ -0,0 +1,34 @@
+package pack_shruti_seasons;
+
+import java.util.Scanner;
+
+public class Season {
+    public static void main(String[] args) {
+        int month;
+        System.out.println("enter any month number: ");
+        Scanner scan = new Scanner(System.in);
+        month=scan.nextInt();
+        if(month==3 || month==4 || month==5)
+        {
+            System.out.println("Summer \uD83D\uDD06 ");
+        } else if (month==6 || month==7 || month==8) {
+            System.out.println("Rainy ☔\uD83C\uDF27 ");
+            
+        }
+        else if (month==9 || month==10 || month==11) {
+            System.out.println("Autumn \uD83C\uDF41 ");
+
+        }
+        else if (month==12 || month==1 || month==2) {
+            System.out.println("Winter \uD83C\uDF84 ");
+
+        }
+        else
+        {
+            System.out.println("Invalid choice ⚠");
+        }
+
+
+
+    }
+}

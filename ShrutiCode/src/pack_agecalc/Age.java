50045f1c825d7b4c9442babd336defbce4186e42
Shruti Deshmukh <147811880+shruti15deshmukh@users.noreply.github.com>
2023-11-30 10:31:56 +0530

    function passing values 10:31 30/11

---
+++ ShrutiCode/src/pack_agecalc/Age.java
@@ -0,0 +1,20 @@
+//package pack_agecalc;
+//
+//import java.time.DateTimeException;
+//import java.time.LocalDateTime;
+//import java.util.Scanner;
+//
+//public class Age {
+//    public void showDob(){
+//        LocalDateTime current= LocalDateTime.now();
+//        System.out.println("enter date of birth: ");
+//        Scanner scan= new Scanner(System.in);
+////        LocalDateTime dob;
+//        String dob=scan.next();
+//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+//
+//        Date dob = sdf.parse(dob);
+//        System.out.println(current-dob);
+//
+//    }
+//}

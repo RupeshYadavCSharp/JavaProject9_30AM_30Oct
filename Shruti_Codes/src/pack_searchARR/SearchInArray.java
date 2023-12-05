93fcbdb6f04a50b6baefbd5be51ff67b79964429
Shruti Deshmukh <147811880+shruti15deshmukh@users.noreply.github.com>
2023-12-06 00:18:32 +0530

    array-3 todos 13:43 5/12

--- Shruti_Codes/src/pack_searchARR/SearchInArray.java
+++ Shruti_Codes/src/pack_searchARR/SearchInArray.java
@@ -3,23 +3,40 @@ package pack_searchARR;
 import java.util.Scanner;
 
 public class SearchInArray {
+    public static int Search(int num,int arr[])
+    {
+        for (int i = 0; i < 8; i++) {
+            if (num == arr[i]) {
+                return i;
+            }
+
+
+        }
+        return -1;
+
+    }
+
     public static void main(String[] args) {
-        int arr[], num;
+        int num, arr[];
         arr=new int[8];
-        Scanner scan=new Scanner(System.in);
-        System.out.println("enter array of 8 elements: ");
+        System.out.println("enter 8 elements of an array: ");
+        Scanner scan= new Scanner(System.in);
         for (int i = 0; i < 8; i++) {
             arr[i]=scan.nextInt();
 
         }
-        System.out.println("enter number you want to search? ");
-        num=scan.nextInt();
-        for (int i = 0; i < 8; i++) {
-            if(arr[i]==num)
-            {
-                System.out.println("number is found in array at"+ i);
-            }
-
+        System.out.println("enter num you want to search: ");
+        num= scan.nextInt();
+        int index=Search(num,arr);
+        if(index==-1)
+        {
+            System.out.println("not");
         }
+        else{
+            System.out.println("found at "+ index);
+        }
+
     }
+
+
 }
